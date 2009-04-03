d5c21239790c0533a2a4291609128828cf42f64a
Andres Jaan Tack <ajtack@gmail.com>
2009-04-03 11:22:15 -0500

    Without condition variables, implemented the correct control flow for condition variables in transactions.

--- main.cxx
+++ main.cxx
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <stack>
 
 using std::cout;
 using std::endl;
+using std::stack;
+
+typedef void* label;
 
 void f();
-void g_prime();
+void g_prime(bool&, stack<label>&);
 void g();
-void h_prime();
+void h_prime(bool&, stack<label>&);
 void h();
 
 int main()	{
@@ -21,49 +25,87 @@ void f()
 	cout << "\t..." << endl;
 	cout << "\t__tm_atomic" << endl;
 	cout << "\t{" << endl;
-	__tm_atomic
-	{
+//	__tm_atomic
+//	{
+		bool waiting = false;
+		stack<label> jump_points;
 		cout << "\t\t..." << endl;
-		g_prime();
+		g_prime(waiting, jump_points);
+		
+		while (waiting)	{
+			cout << "\t\twait(...)" << endl;
+			g_prime(waiting, jump_points);
+		}
 		cout << "\t\t..." << endl;
-	}
+//	}
 	cout << "\t}" << endl;
 	cout << "\t..." << endl;
 	cout << "}" << endl;
 }
 
-__attribute__((tm_callable)) void g_prime()
+__attribute__((tm_callable)) void g_prime(bool& am_waiting, stack<label>& jump_points)
 {
-	cout << "\t\tg'() {" << endl;
-	cout << "\t\t\t..." << endl;
-	h_prime();
+	if (am_waiting)
+		goto *jump_points.top();
+	else	{
+		cout << "\t\tg'() {" << endl;
+		cout << "\t\t\t..." << endl;
+	}
+	
+	h_prime(am_waiting, jump_points);
+	
+	if (am_waiting)	{
+		jump_points.push(&&after_wait);
+		return;
+	}
+	else	{
+		goto after_h_prime;
+	}
+after_wait:
+	jump_points.pop();
+	h_prime(am_waiting, jump_points);
+
+after_h_prime:
 	cout << "\t\t\t..." << endl;
 	cout << "\t\t}" << endl;
 }
 
 void g()
 {
-	cout << "\t\tg() {" << endl;
-	cout << "\t\t\t..." << endl;
+	cout << "\tg() {" << endl;
+	cout << "\t\t..." << endl;
 	h();
-	cout << "\t\t\t..." << endl;
-	cout << "\t\t}" << endl;
+	cout << "\t\t..." << endl;
+	cout << "\t}" << endl;
 }
 
-__attribute__((tm_callable)) void h_prime()
+__attribute__((tm_callable)) void h_prime(bool& am_waiting, stack<label>& jump_points)
 {
-	cout << "\t\t\th'() {" << endl;
-	cout << "\t\t\t\t..." << endl;
-	cout << "\t\t\t\t// do some work." << endl;
+	if (am_waiting)
+		goto *jump_points.top();
+	else	{
+		cout << "\t\t\th'() {" << endl;
+		cout << "\t\t\t\t..." << endl;
+	}
+
+	// In place of the wait().
+	jump_points.push(&&finish);
+	am_waiting = true;
+	return;
+	
+finish:
+	jump_points.pop();
+	am_waiting = false;
 	cout << "\t\t\t\t..." << endl;
 	cout << "\t\t\t}" << endl;
+	return;
 }
 
 void h()
 {
-	cout << "\t\t\th() {" << endl;
-	cout << "\t\t\t\t..." << endl;
-	cout << "\t\t\t\t// do some work." << endl;
-	cout << "\t\t\t\t..." << endl;
-	cout << "\t\t\t}" << endl;
+	cout << "\t\th() {" << endl;
+	cout << "\t\t\t..." << endl;
+	cout << "\t\t\t// do some work." << endl;
+	cout << "\t\t\t..." << endl;
+	cout << "\t\t}" << endl;
 }
