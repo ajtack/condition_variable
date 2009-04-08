3434cd17b187ede529fba3cc0dcec0af8a27e30b
Andres Jaan Tack <ajtack@gmail.com>
2009-04-08 16:28:29 -0500

    Generalized some of the localized transformations of wait() calls and of waitable function calls.

--- main.cxx
+++ main.cxx
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <stack>
+#include <setjmp.h>
 
 using std::cout;
 using std::endl;
 using std::stack;
+
+#define tm_begin cout << "\t__tm_atomic" << endl; \
+                 cout << "\t{" << endl;
+
+#define tm_end   cout << "\t}" << endl;
 
 typedef void* label;
 
@@ -20,56 +26,75 @@ int main()	{
 void f()
 {
 	cout << "f() {" << endl;
-	cout << "\t..." << endl;
+	cout << "\t// before g ..." << endl;
 	g();
-	cout << "\t..." << endl;
-	cout << "\t__tm_atomic" << endl;
-	cout << "\t{" << endl;
-//	__tm_atomic
-//	{
+	cout << "\t// after g ..." << endl;
+	
+	// In place of a regular call to g' (which would be as above)
+	{
 		bool waiting = false;
+		bool first_time = true;
 		stack<label> jump_points;
-		cout << "\t\t..." << endl;
-		g_prime(waiting, jump_points);
 		
-		while (waiting)	{
-			cout << "-------------------------------------------" << endl;
-			cout << "\t}" << endl;
-			cout << "\twait(...)" << endl;
-			cout << "\t__tm_atomic {" << endl;
-			cout << "-------------------------------------------" << endl;
-			g_prime(waiting, jump_points);
-		}
-		cout << "\t\t..." << endl;
-//	}
-	cout << "\t}" << endl;
+		do {
+			tm_begin	{
+				if (first_time)
+					cout << "\t\t// before g' ..." << endl;
+				
+				g_prime(waiting, jump_points);
+				
+				if (!waiting)
+					cout << "\t\t// after g' ..." << endl;
+			}
+			tm_end
+			
+			if (waiting)
+				cout << "\twait(...)" << endl;
+			
+			first_time = false;
+		} while(waiting);
+	}
+
 	cout << "\t..." << endl;
 	cout << "}" << endl;
 }
 
 __attribute__((tm_callable)) void g_prime(bool& am_waiting, stack<label>& jump_points)
 {
-	if (am_waiting)
-		goto *jump_points.top();
+	if (am_waiting)	{
+		label continuation = jump_points.top();
+		jump_points.pop();
+		goto *continuation;
+	}
 	else	{
 		cout << "\t\tg'() {" << endl;
 		cout << "\t\t\t..." << endl;
 	}
 	
-	h_prime(am_waiting, jump_points);
-	
-	if (am_waiting)	{
-		jump_points.push(&&after_wait);
-		return;
-	}
-	else	{
-		goto after_h_prime;
-	}
-after_wait:
-	jump_points.pop();
-	h_prime(am_waiting, jump_points);
+	// Call to h' is replaced with...
+	waitable_function_call_1:
+	{
+		h_prime(am_waiting, jump_points);
 
-after_h_prime:
+		if (am_waiting)	{
+			jump_points.push(&&waitable_function_call_1);
+			return;
+		}
+	}
+	
+	cout << "\t\t\t..." << endl;
+	
+	// Call to h' is replaced with...
+	waitable_function_call_2:
+	{
+		h_prime(am_waiting, jump_points);
+
+		if (am_waiting)	{
+			jump_points.push(&&waitable_function_call_2);
+			return;
+		}
+	}
+
 	cout << "\t\t\t..." << endl;
 	cout << "\t\t}" << endl;
 }
@@ -85,21 +110,38 @@ void g()
 
 __attribute__((tm_callable)) void h_prime(bool& am_waiting, stack<label>& jump_points)
 {
-	if (am_waiting)
-		goto *jump_points.top();
+	if (am_waiting)	{
+		label continuation = jump_points.top();
+		jump_points.pop();
+		goto *continuation;
+	}
 	else	{
 		cout << "\t\t\th'() {" << endl;
 		cout << "\t\t\t\t..." << endl;
 	}
 
-	// In place of the wait().
-	jump_points.push(&&finish);
-	am_waiting = true;
-	return;
+	// In place of the wait() ...
+	{
+		jump_points.push(&&after_wait_1);
+		am_waiting = true;
+		return;
+		
+	after_wait_1:
+		am_waiting = false;
+	}
 	
-finish:
-	jump_points.pop();
-	am_waiting = false;
+	cout << "\t\t\t\t..." << endl;
+	
+	// In place of the wait() ...
+	{
+		jump_points.push(&&after_wait_2);
+		am_waiting = true;
+		return;
+		
+	after_wait_2:
+		am_waiting = false;
+	}
+	
 	cout << "\t\t\t\t..." << endl;
 	cout << "\t\t\t}" << endl;
 	return;
